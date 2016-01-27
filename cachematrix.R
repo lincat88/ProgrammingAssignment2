9034ea5f8b7e2ade6a66b8c9bc94439488b933f0
lincat88 <lindsay.f.cattell@gmail.com>
2016-01-27 07:17:10 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together these functions set and store a matrix and its inverse 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix is a list of functions that stores and reads a matrix and its inverse
+makeCacheMatrix<-function(x=matrix()) {
+        m<-NULL
+        set<-function(y) { ##modifies an existing object
+                x<<-y ##these are global so that they work in the get/setmean/getmean funcs below
+                m<<-null
+        }
+        get<-function() x ##function returning the original matrix
+        setinverse<-function(solve) m<<-solve ##function for setting the inverse of the matrix
+        getinverse<-function() m ##function for returning the inverse of the matrix
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) ##this is a list of functions
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve checks to see if the inverse was already calculated and stored, if not, it does those things
+
+cacheSolve<-function(x,...) {
+        ##return a matrix that is the inverse of x
+        m<- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setinverse(m)
+        m
 }
+
+##code to test whether my code is working properly, expect the negative signs to switch cols
+matrix1<-matrix(c(-1,-2,1,1),2,2)
+x<-makeCacheMatrix(matrix1)
+x$get()
+cacheSolve(x)
+
+
